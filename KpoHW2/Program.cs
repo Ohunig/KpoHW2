5d239f3c1c9340d1bb929b455495b8ff7d3722d0
Ohunig <kkozachenkovitaliy@yandex.ru>
2025-11-08 17:25:33 +0300

    Fix bug in program.cs

--- KpoHW2/Program.cs
+++ KpoHW2/Program.cs
@@ -1,12 +1,9 @@
 ﻿using KpoHW2;
-using KpoHW2.Application.Facades;
 using KpoHW2.Presentation;
 using KpoHW2.Presentation.MainScreen;
 using Microsoft.Extensions.DependencyInjection;
 
 var serviceProvider = CompositionRoot.Services;
-
-BankAccountFacade facade = serviceProvider.GetRequiredService<BankAccountFacade>();
 
 NavigationController controller = serviceProvider.GetRequiredService<NavigationController>();
 controller.Push(MainScreenBuilder.Build());
